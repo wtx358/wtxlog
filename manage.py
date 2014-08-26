7349fd34c2e51f19390fbc39376bfa9875a31117
digwtx <wtx358@qq.com>
2014-08-26 20:14:10 +0800

    Added robots.txt

--- manage.py
+++ manage.py
@@ -16,7 +16,6 @@ for deps_path in deps_paths:
 from app import create_app, db, get_appconfig
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
-from flask import send_from_directory
 
 # bae, sae, production, local(default)
 app = create_app(os.getenv('APP_CONFIG') or str(get_appconfig()) or 'default')
@@ -25,12 +24,6 @@ manager = Manager(app)
 migrate = Migrate(app, db)
 
 manager.add_command('db', MigrateCommand)
-
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
 @manager.command
