33b140c26f3a081d79f5e6480d379b28496a98e7
digwtx <wtx358@qq.com>
2014-09-15 19:07:33 +0800

    移除 APPENV 配置值
    这个配置值不再使用

--- wtxlog/__init__.py
+++ wtxlog/__init__.py
@@ -38,7 +38,6 @@ def get_appconfig():
 
 def create_app(config_name):
     app = Flask(__name__)
-    app.config['APPENV'] = str(get_appconfig())
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
