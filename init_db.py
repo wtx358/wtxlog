b5add18381a61ebe893d2c8c42f19091aa297735
digwtx <wtx358@qq.com>
2014-12-03 19:40:32 +0800

    Add a Database and Category Initial Script

---
+++ init_db.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+"""
+Just for development time
+"""
+
+from manage import app, db
+from wtxlog.models import User, Role, Category
+
+if __name__ == '__main__':
+    # init database
+    db.create_all()
+
+    # create user roles
+    Role.insert_roles()
+
+    # add administrator
+    user = User()
+    user.username = 'admin'
+    user.password = 'admin'
+    user.email = 'admin@example.com'
+    user.role = Role.query.filter_by(permissions=0xff).first()
+    user.confirmed = True
+
+    # add default category
+    category = Category()
+    category.slug = 'default'
+    category.name = 'Default'
+
+    # commit
+    db.session.add(user)
+    db.session.add(category)
+    db.session.commit()
+
+    print '=================================='
+    print 'Init Done.'
+    print '=================================='
