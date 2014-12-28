8b1de4d926b493ebb905395cb553399ed8b7dff6
digwtx <wtx358@qq.com>
2014-12-28 10:41:20 +0800

    manage增加test指令，并使用coverage制作报告

--- manage.py
+++ manage.py
@@ -13,6 +13,12 @@ for deps_path in deps_paths:
     if deps_path not in sys.path:
         sys.path.insert(0, deps_path)
 
+COV = None
+if os.environ.get('FLASK_COVERAGE'):
+    import coverage
+    COV = coverage.coverage(branch=True, include='wtxlog/*')
+    COV.start()
+
 from wtxlog import create_app, db, get_appconfig
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
@@ -49,6 +55,28 @@ def make_shell_context():
         Link=Link, FriendLink=FriendLink, Flatpage=Flatpage,
         Label=Label, Redirect=Redirect, Setting=Setting
     )
+
+
+@manager.command
+def test(coverage=False):
+    """Run the unit tests."""
+    if coverage and not os.environ.get('FLASK_COVERAGE'):
+        import sys
+        os.environ['FLASK_COVERAGE'] = '1'
+        os.execvp(sys.executable, [sys.executable] + sys.argv)
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+    if COV:
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'tmp', 'coverage')
+        COV.html_report(directory=covdir)
+        print('HTML version: file://%s/index.html' % covdir)
+        COV.erase()
 
 
 if __name__ == '__main__':
