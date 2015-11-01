81c19fdf443e7e79722ddd4fb34be361f0f7c436
digwtx <wtx358@qq.com>
2015-11-01 22:22:55 +0800

    更新版权年份

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'wtxlog'
-copyright = u'2014, digwtx'
+copyright = u'2014-2015, digwtx'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
